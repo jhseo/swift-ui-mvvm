ea52e243de7fb631208c2ec583566df8270fbd0e
jhSeo <jh.seo@battleent.com>
2020-02-06 11:33:31 +0900

    Add ImageView for remote image

---
+++ swift-ui-mvvm/View/Component/ImageView.swift
@@ -0,0 +1,30 @@
+//
+//  ImageView.swift
+//  swift-ui-mvvm
+//
+//  Created by jhseo on 2020/02/06.
+//  Copyright © 2020 jhseo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ImageView: View {
+    @ObservedObject var imageLoader: ImageLoader
+    @State var image: UIImage = UIImage()
+
+    init(imageURL: String) {
+        imageLoader = ImageLoader(imageURL: imageURL)
+    }
+
+    var body: some View {
+        VStack {
+            Image(uiImage: imageLoader.data.isEmpty ? image : UIImage(data: imageLoader.data)!)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50, height: 50)
+        }
+        .onReceive(imageLoader.didChange) { data in
+            self.image = UIImage(data: data) ?? UIImage()
+        }
+    }
+}
