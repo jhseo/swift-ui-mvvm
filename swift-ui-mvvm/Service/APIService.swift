27a535085677c77e6ea4c98f7ac37cf10eff9f6e
jhSeo <jh.seo@battleent.com>
2020-02-06 11:25:51 +0900

    Add APIService

---
+++ swift-ui-mvvm/Service/APIService.swift
@@ -0,0 +1,33 @@
+//
+//  APIService.swift
+//  swift-ui-mvvm
+//
+//  Created by jhseo on 2020/02/06.
+//  Copyright © 2020 jhseo. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class APIService {
+    private static let baseURL: String = "https://www.googleapis.com/books/v1/volumes"
+
+    static func searchBook(searchText: String, startIndex: Int, maxResults: Int) -> AnyPublisher<BookModel?, Never> {
+        var urlComponents = URLComponents(string: baseURL)!
+        urlComponents.queryItems = [
+            URLQueryItem(name: "q", value: searchText),
+            URLQueryItem(name: "startIndex", value: String(startIndex)),
+            URLQueryItem(name: "maxResults", value: String(maxResults))
+        ]
+        var request = URLRequest(url: urlComponents.url!)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        return URLSession.shared.dataTaskPublisher(for: request)
+            .map { $0.data }
+            .decode(type: BookModel.self, decoder: JSONDecoder())
+            .map { $0 }
+            .replaceError(with: nil)
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
+}
