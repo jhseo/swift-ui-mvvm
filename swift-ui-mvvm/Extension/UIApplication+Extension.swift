df9584b3f9f900a07949257012392576ed4281ff
jhSeo <jh.seo@battleent.com>
2020-02-06 12:17:24 +0900

    Add drag gesture for resign keyboard

---
+++ swift-ui-mvvm/Extension/UIApplication+Extension.swift
@@ -0,0 +1,33 @@
+//
+//  UIApplication+Extension.swift
+//  swift-ui-mvvm
+//
+//  Created by jhseo on 2020/02/06.
+//  Copyright © 2020 jhseo. All rights reserved.
+//
+
+import SwiftUI
+
+extension UIApplication {
+    func endEditing(_ force: Bool) {
+        self.windows
+            .filter{$0.isKeyWindow}
+            .first?
+            .endEditing(force)
+    }
+}
+
+struct ResignKeyboardOnDragGesture: ViewModifier {
+    var gesture = DragGesture().onChanged{_ in
+        UIApplication.shared.endEditing(true)
+    }
+    func body(content: Content) -> some View {
+        content.gesture(gesture)
+    }
+}
+
+extension View {
+    func resignKeyboardOnDragGesture() -> some View {
+        return modifier(ResignKeyboardOnDragGesture())
+    }
+}
