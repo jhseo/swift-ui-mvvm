77a5c66157d70fac8ce4ff9c3808bd70ea383e0c
jhSeo <jh.seo@battleent.com>
2020-02-06 11:12:59 +0900

    Add a default project

---
+++ swift-ui-mvvmTests/swift_ui_mvvmTests.swift
@@ -0,0 +1,34 @@
+//
+//  swift_ui_mvvmTests.swift
+//  swift-ui-mvvmTests
+//
+//  Created by jhseo on 2020/02/06.
+//  Copyright © 2020 jhseo. All rights reserved.
+//
+
+import XCTest
+@testable import swift_ui_mvvm
+
+class swift_ui_mvvmTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
